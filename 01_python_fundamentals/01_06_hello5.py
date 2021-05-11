beb4b10ce0b9c63521a16a7d2a37d9dcb3d9d410
Ben Brauer <12072267+bbrauer100@users.noreply.github.com>
2021-05-11 16:57:43 -0700

    initial commit

---
+++ 01_python_fundamentals/01_06_hello5.py
@@ -0,0 +1,14 @@
+'''
+
+Write the necessary code to display "Hello World!" 5 times.
+
+
+'''
+
+message = "hello world"
+
+count = 0
+for i in message:
+    x = x + 1
+    print("hello world"[0:x])
+
