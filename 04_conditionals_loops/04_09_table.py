5d46e28496400c8e74800298900a1a805601f40e
Ben Brauer <bens_mac@Bens-MacBook-Pro.local>
2021-05-21 05:52:56 -0700

    More labs! These should be it for the data types and conditional loops

--- 04_conditionals_loops/04_09_table.py
+++ 04_conditionals_loops/04_09_table.py
@@ -8,5 +8,8 @@ Use a loop to print the following table to the console:
  40 41 42 43 44 45 46 47 48 49
 
 '''
+for x in range(0, 50, 10):
+    print(x, x + 1, x + 2, x + 3, x + 4, x + 5, x + 6, x + 7, x + 8, x + 9)
 
-
+# for i in range(0, 50, 10):
+#     print(' '.join([str(j) for j in range(i, i + 10)]))
--- 04_conditionals_loops/04_10_squares.py
+++ 04_conditionals_loops/04_10_squares.py
@@ -5,3 +5,11 @@ Use a for loop that demonstrates the use of the range function.
 
 '''
 
+for i in range(1,51):
+    print(i**2)
+
+
+# x = 1
+# while x in range(1,51):
+#     print(x**2)
+#     x += 1
